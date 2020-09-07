49e0c00fe3a407a1558de339d23e610f6888d16d
i5x64BIT <vaintomer@gmail.com>
2020-09-07 22:50:36 +0300

    app.js / route extra option was removed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ip = 'localhost';
 mongoose.connect('mongodb://localhost/shoe_store');
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes/index'), {});
 app.use('/shoes', require('./routes/shoes'));
 
 app.listen(port, ip, () => console.log(`App listening on port ${port}!`));
